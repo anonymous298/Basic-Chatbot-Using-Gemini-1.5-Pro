71423ed1001bfa5f6aa040590aa139d0c065769d
anonymous298 <tackletalha@gmail.com>
2025-02-02 23:27:24 +0500

    changes

--- basic_chatbot_using_gemini_1-5_pro.py
+++ basic_chatbot_using_gemini_1-5_pro.py
@@ -9,18 +9,20 @@ from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
+# Loading the API Key from Google Gemini
 SEC_KEY = os.getenv('GOOGLE_GEMINI_API')
 
 st.title('Basic Chatbot Using Google Gemini 1.5 pro')
 user_input = st.text_input('What do you want to ask? ')
 
-# loading LLM as api
+# loading Our LLM
 llm = ChatGoogleGenerativeAI(
     model='gemini-1.5-pro-latest',
     api_key=SEC_KEY,
     temperature=0.3
 )
 
+# Creating a ChatPromptTemplate for our LLM
 prompt = ChatPromptTemplate.from_messages(
     [
         ('system', 'You are an Intelligent Assistant Jarvis'),
@@ -28,8 +30,10 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
+# Creating a Chain for our ChatBot
 chain = prompt | llm | StrOutputParser()
 
+
 if st.button('Ask'):
-    response = chain.invoke({'question' : user_input})
+    response = chain.invoke({'question' : user_input}) # Invoking the Chain with the users input
     st.write(response)
